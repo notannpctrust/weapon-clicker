51915a6a7e29720d7dabe535070c3ac5f4d95464
Christopher Brown <cbrown2@brooklynsteamcenter.org>
2025-06-03 14:50:21 -0400

    june 2

---
+++ src/main.js
@@ -0,0 +1,32 @@
+let points = 1000
+
+let pointCounter = document.getElementById("pointCounter")
+
+let kunaiLevel = 0
+
+let kLevel = document.getElementById('klevel')
+
+function changePoint(amount){
+  points = points + amount
+
+
+  console.log("points")
+
+
+  pointCounter.innerText = "Damage Built up:"  + points
+  
+
+}
+
+function buyKunai(){
+      if (points >= 100) {
+        changePoint(-100)
+      kunaiLevel = kunaiLevel + 1
+      kLevel.innerText = "Level:" + kunaiLevel
+    
+}    else {
+         alert("get ur dmg up not ur money up ")
+           
+}
+
+}
